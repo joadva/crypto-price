db1bdf9cccfcfcbf8bf0ffcfa2db3e49aa947076
Jose Adrian Valentin Rodriguez <joadva@hotmail.com>
2021-06-22 11:31:34 -0500

    ap lista acutalzia precios y hace busqueda :3

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
+import { CLIEngine } from 'eslint';
 import React, {useEffect,useState} from 'react'
 import { View, Text,FlatList ,StyleSheet, TextInput, StatusBar} from 'react-native'
 import CoinItem from './components/CoinItem';
 const App = () => {
 
   const [coins, setCoins] = useState([])
-
+  const [search, setSearch] = useState('')
+  const [refreshing, setRefreshing] = useState(false)
   const loadData = async () => {
     const   res = await fetch(
        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -25,21 +27,34 @@ const App = () => {
   return (
     <View style={styles.container}>
     <StatusBar backgroundColor="#141414" />
-      <View style={styles.header}>
-        <Text style={styles.title}>Crypto View</Text>
-        <TextInput style={styles.searchInput}/>
-      </View>
-     
-      <FlatList
-      style={styles.list}
-      data={coins}
-      renderItem={({item}) => {
-        return <CoinItem coin={item} />
-      }}
-      showsVerticalScrollIndicator={false}
-      >
-      </FlatList>
+
+    <View style={styles.header}>
+      <Text style={styles.title}>CryptoMarket</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search a Coin"
+        placeholderTextColor="#858585"
+        onChangeText={(text) => text && setSearch(text)}
+      />
     </View>
+
+    <FlatList
+      style={styles.list}
+      data={coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+          coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      )}
+      showsVerticalScrollIndicator={false}
+      renderItem={({ item }) => <CoinItem coin={item} />}
+      refreshing={refreshing}
+      onRefresh={async () => {
+        setRefreshing(true);
+        await loadData();
+        setRefreshing(false);
+      }}
+    />
+  </View>
   )
 }
 
